8ecf483af84fde228ad951c78abb36541aa7977b
danbe <danbe8080@gmail.com>
2022-02-11 17:31:34 +0900

    To Do Setup

--- src/theme.ts
+++ src/theme.ts
@@ -5,9 +5,3 @@ export const darkTheme:DefaultTheme = {
 	textColor: "#f5f6fa",
 	accentColor: "#12CBC4"
 }
-
-export const lightTheme:DefaultTheme = {
-	bgColor: "#f5f6fa",
-	textColor: "#2f3640",
-	accentColor: "#12CBC4"
-}
