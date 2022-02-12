8345e7a3ff6ed83b44008d579eac53a2c352482d
danbe <danbe8080@gmail.com>
2022-02-12 09:07:11 +0900

    validate, react-form-hooks Done

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -8,6 +8,7 @@ interface IForm {
   username: string;
   password: string;
   password1: string;
+  extraError?:string;
 }
 
 function ToDoList() {
@@ -15,13 +16,21 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
+    setError /* 특정한 error를 발생시키게 해줌 */
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if(data.password !== data.password1) {
+      setError(
+        "password1", 
+        {message:"password are not the same"}, 
+        {shouldFocus: true} /* 커서가 즉시 에러난 입력창으로 이동 */
+        );
+    }
+    // setError("extraError", {message: "Server offline"}); /* form 전체의 error */
   }
   console.log(errors)
   return (
@@ -41,7 +50,19 @@ function ToDoList() {
         />
         <span>{errors?.email?.message}</span>
         <input
-          {...register("firstName", { required: "write here" })}
+          {...register(
+            "firstName", 
+              { 
+              required: "write here", 
+              validate: {
+                noNico: (value) => 
+                value.includes("nico") ? "no nicos allowed" : true ,
+                /* "nico"라는 단어가 들어가면 error (return boolean)*/
+                /* async await(비동기)로 서버와 확인하고 응답 받을 수 있음 */
+              }
+              }
+            )
+          }
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message}</span>
@@ -68,6 +89,7 @@ function ToDoList() {
           placeholder="Password1"/>
         <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
