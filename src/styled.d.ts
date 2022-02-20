95040b86fa6018e3b4357a97b3c8cc6517d022b0
danbe <danbe8080@gmail.com>
2022-02-20 22:33:45 +0900

    Framer Motion 초기

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,9 +2,5 @@ import 'styled-components';
 
 // and extend theme!
 declare module 'styled-components' {
-	export interface DefaultTheme {
-		bgColor: string;
-		cardColor: string;
-		boardColor: string;
-	}
+	export interface DefaultTheme {}
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme:DefaultTheme = {
-	bgColor: "#fdcb6e",
-	cardColor: "#f1f2f6",
-	boardColor: "#f6b93b",
-}
+export const darkTheme:DefaultTheme = {}
